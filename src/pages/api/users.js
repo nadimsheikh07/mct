9c5ea39ad1a671a1275a580ed56d2b6ac0d4efd1
= <nadim.sheikh.07@gmail.com>
2023-05-03 20:14:19 +0530

    delete updated

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -7,18 +7,29 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
-    const { id } = req.query;
-    const collection = req.db.collection('users')
-    let doc = {}
+    try {
+        const { id } = req.query;
+        const collection = req.db.collection('users')
+        let data = null
+        if (id) {
+            data = await collection.findOne({ _id: new ObjectId(id) })
+        } else {
+            data = await collection.find({})
+                .toArray();
+        }
 
-    if (id) {
-        doc = await collection.findOne({_id:new ObjectId(id)})
-    } else {
-        doc = await collection.find({})
-            .toArray();
+        return res.json({
+            message: 'data get successfully',
+            success: true,
+            data: data
+        });
+    } catch (error) {
+        // return an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
     }
-
-    res.json(doc)
 });
 
 handler.post(async (req, res) => {
@@ -41,10 +52,29 @@ handler.post(async (req, res) => {
 
 handler.put(async (req, res) => {
     try {
+        const { id } = req.query;
         let input = req.body
-        let data = await req.db.collection('users').updateOne(input)
+        let data = await req.db.collection('users').updateOne({ _id: new ObjectId(id) }, { $set: input })
         return res.json({
-            message: 'data added successfully',
+            message: 'data updated successfully',
+            success: true,
+            data: data
+        });
+    } catch (error) {
+        // return an error
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        });
+    }
+})
+
+handler.delete(async (req, res) => {
+    try {
+        const { id } = req.query;
+        let data = await req.db.collection('users').deleteOne({ _id: new ObjectId(id) })
+        return res.json({
+            message: 'data deleted successfully',
             success: true,
             data: data
         });
